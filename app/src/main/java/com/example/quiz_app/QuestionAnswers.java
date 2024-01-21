be41062cc448f1a208ecb3fc90363d7a5023f699
Ravindragouda1824 <ravindragoudaspatil2004@gmail.com>
2024-01-21 17:29:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswers.java
@@ -0,0 +1,27 @@
+package com.example.quiz_app;
+
+public class QuestionAnswers {
+    public static String questions[] = {
+            "Which company owns android?",
+            "Which one is not a programming language?",
+            "Where are you watching this video?",
+            "Where is the Main Office of Apple?"
+
+    };
+
+    public static String choices[][] = {
+            {"Google", "Apple", "Nokia", "Samsung"},
+            {"Java", "Kotlin", "MongoDB", "Python"},
+            {"Facebook", "Instagram", "Whatsapp", "Youtube"},
+            {"Mexico","San Francisco","California","Florida"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "MongoDB",
+            "Youtube",
+            "California"
+
+    };
+}
